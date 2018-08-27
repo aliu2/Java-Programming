0cf87a6bb4dcdf2a0bfd80dbb4be27fb70e015fa
Uzair Ali <34479006+aliu2@users.noreply.github.com>
2018-08-27 18:30:49 +0100

    Create ForTenToOne.java

---
+++ Week 2/Loops/For-Loops/ForTenToOne.java
@@ -0,0 +1,11 @@
+/* prints the numbers from 10 to 1 inclusive using a for loop */
+public class ForTenToOne
+{
+	public static void main(String [] args)
+	{
+		for (int i = 10; i > 0; i--)
+		{
+			System.out.print(i + " ");
+		}
+	}
+}
