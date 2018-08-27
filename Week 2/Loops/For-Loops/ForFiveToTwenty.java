75239def39b59b9a11be0a2be29d3bdc20b6948b
Uzair Ali <34479006+aliu2@users.noreply.github.com>
2018-08-27 18:29:55 +0100

    Create ForFiveToTwenty.java

---
+++ Week 2/Loops/For-Loops/ForFiveToTwenty.java
@@ -0,0 +1,11 @@
+/* print the numbers from 5 to 20 inclusive using a for loop */
+public class ForFiveToTwenty
+{
+	public static void main(String [] args)
+	{
+		for (int i = 5; i <= 20; i++)
+		{
+			System.out.print(i + " ");
+		}
+	}
+}
