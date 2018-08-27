8b56351fc44a9b10a1d57742ea21b0c99cfcbab6
Uzair Ali <34479006+aliu2@users.noreply.github.com>
2018-08-27 19:52:59 +0100

    Create Cat.java

---
+++ Week 5/Classes-And-Interfaces/Greetings/Cat.java
@@ -0,0 +1,19 @@
+public class Cat implements Polite
+{
+	private String name;
+
+	public Cat(String n)
+	{
+		name = n;
+	}
+
+	public String toString()
+	{
+		return "cat: " + name;
+	}
+
+	public void hello()
+	{
+		System.out.println("Meow");
+	}
+}
