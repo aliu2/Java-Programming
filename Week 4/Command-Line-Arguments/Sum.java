7da4fcd7d5ba89795f014198d700be8a1c8f76c5
Uzair Ali <34479006+aliu2@users.noreply.github.com>
2018-08-27 19:42:50 +0100

    Create Sum.java

---
+++ Week 4/Command-Line-Arguments/Sum.java
@@ -0,0 +1,12 @@
+public class Sum
+{
+	public static void main(String[] args)
+	{
+		int sum = 0;
+		for (String s: args)
+		{
+			sum += Integer.parseInt(s);
+		}
+		System.out.println(sum);
+	}
+}
